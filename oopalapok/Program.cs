b305ada890e317fef2f24fd93a97852a79983352
piringerm <piringerm@301-15>
2025-09-10 09:25:05 +0200

    Feladat3 is ready

--- oopalapok/Program.cs
+++ oopalapok/Program.cs
@@ -10,28 +10,44 @@ namespace oopalapok
 {
     public class szemely
     {
-        private string nev;
-        private int kor;
+        public string nev { get; set; }
+        public int kor { get; set; }
+/*
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+        public int Kor
+        {
+            get { return kor; }
+            set { kor = value; }
+        }
 
-        public szemely(string Nev, int Kor)
-        {
-            nev = Nev;
-            kor = Kor;
-        }
-        public string kiir()
-        {
-            return $"A tanuló neve: {nev} és {kor} éves";
-        }
+          public szemely(string Nev, int Kor)
+          {
+              nev = Nev;
+              kor = Kor;
+          }
+          public string kiir()
+          {
+              return $"A tanuló neve: {nev} és {kor} éves";
+          }*/
 
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            szemely tanulo1 = new szemely("Mark", 19);
-            Console.WriteLine(tanulo1.kiir());
-            szemely tanulo2 = new szemely("John", 21);
-            Console.WriteLine(tanulo2.kiir());
+            szemely tanulo1 = new szemely();
+            tanulo1.Nev = "John";
+            Console.WriteLine(tanulo1.Nev);
+            tanulo1.Kor = 23;
+            Console.WriteLine(tanulo1.Kor);
+            /*  szemely tanulo1 = new szemely("Mark", 19);
+              Console.WriteLine(tanulo1.kiir());
+              szemely tanulo2 = new szemely("John", 21);
+              Console.WriteLine(tanulo2.kiir());*/
             // Console.WriteLine($"A tanuló neve: {tanulo.nev} és {tanulo.kor} éves");
         }
     }
