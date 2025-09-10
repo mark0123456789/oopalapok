9ef31766777b9cefa00454959402b4771674b705
piringerm <piringerm@301-15>
2025-09-10 09:11:22 +0200

    fealadat2 is ready

--- oopalapok/Program.cs
+++ oopalapok/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,17 +10,30 @@ namespace oopalapok
 {
     public class szemely
     {
-        public string nev = "márk";
-        public int kor =30;
-    }
-    
+        private string nev;
+        private int kor;
+
+        public szemely(string Nev, int Kor)
+        {
+            nev = Nev;
+            kor = Kor;
+        }
+        public string kiir()
+        {
+            return $"A tanuló neve: {nev} és {kor} éves";
+        }
+
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-        szemely tanulo = new szemely();
-        Console.WriteLine($"A tanuló neve: {tanulo.nev} és {tanulo.kor} éves");
-    }
+            szemely tanulo1 = new szemely("Mark", 19);
+            Console.WriteLine(tanulo1.kiir());
+            szemely tanulo2 = new szemely("John", 21);
+            Console.WriteLine(tanulo2.kiir());
+            // Console.WriteLine($"A tanuló neve: {tanulo.nev} és {tanulo.kor} éves");
+        }
     }
 }
+
