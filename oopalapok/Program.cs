0398679c0c9820193c676b928e22057c94efd24b
piringerm <piringerm@301-15>
2025-09-10 08:30:55 +0200

    feladat1 is ready

--- oopalapok/Program.cs
+++ oopalapok/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using oopalapok;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,19 @@ using System.Threading.Tasks;
 
 namespace oopalapok
 {
+    public class szemely
+    {
+        public string nev = "márk";
+        public int kor =30;
+    }
+    
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-        }
+        szemely tanulo = new szemely();
+        Console.WriteLine($"A tanuló neve: {tanulo.nev} és {tanulo.kor} éves");
+    }
     }
 }
