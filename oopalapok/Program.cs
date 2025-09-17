1c89365e158293e7c94654fd0d0c7d7d21c41190
piringerm <piringerm@301-15>
2025-09-17 08:08:51 +0200

    feladat 5 is ready

--- oopalapok/Program.cs
+++ oopalapok/Program.cs
@@ -35,15 +35,50 @@ namespace oopalapok
           }*/
 
     }
+    public class Bankszamla 
+    {
+        private int egyenleg;
+
+        public int Egyenleg 
+        {
+            get { return egyenleg; }
+            set 
+            {
+                if(value>0)
+                egyenleg = value; 
+            else
+                Console.WriteLine("Az egyenleg nem lehetnulla");
+            }
+        }
+
+        private void betesz() 
+        
+        {
+        
+        }
+        private void kivesz() 
+        {
+
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+
+
+
             szemely tanulo1 = new szemely();
             tanulo1.Nev = "John";
             Console.WriteLine(tanulo1.Nev);
             tanulo1.Kor = 23;
             Console.WriteLine(tanulo1.Kor);
+
+
+            Bankszamla szamla1 = new Bankszamla();
+            szamla1.Egyenleg = 1000;
+            Console.WriteLine(szamla1.Egyenleg);
             /*  szemely tanulo1 = new szemely("Mark", 19);
               Console.WriteLine(tanulo1.kiir());
               szemely tanulo2 = new szemely("John", 21);
